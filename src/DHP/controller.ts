208d5b950ee3b93a46dec2726dfc647384c24cf2
hemant-eminds <hemant.sharma@eminds.ai>
2023-10-16 16:02:16 +0530

    add error handling and logs

--- src/DHP/controller.ts
+++ src/DHP/controller.ts
@@ -18,8 +18,16 @@ export const search = async (
   res: Response,
   next: NextFunction
 ) => {
-  const { data } = await searchService(req?.body);
-  return res.status(200).json(data);
+  try {
+    console.log("Request Body for Search Endpoint :: ", JSON.stringify(req?.body));
+    const { data } = await searchService(req?.body);
+    return res.status(200).json(data);
+  } catch(error: any) {
+    console.log("Error for Search Endpoint :: ", error);
+    return res.status(error?.response?.status || 500).json({ 
+      error: error?.response?.statusText || "An exception has occurred."
+    });
+  }
 };
 
 export const select = async (
@@ -28,18 +36,27 @@ export const select = async (
   next: NextFunction
 ) => {
   try {
+    console.log("Request Body for Select Endpoint :: ", JSON.stringify(req?.body));
     const { data } = await selectService(req?.body);
     return res.status(200).json(data);
   } catch(error: any) {
+    console.log("Error for Select Endpoint :: ", error);
     return res.status(error?.response?.status || 500).json({ 
       error: error?.response?.statusText || "An exception has occurred."
     });
   }
-  
 }
 export const init = async (req: Request, res: Response, next: NextFunction) => {
-  const { data } = await initService(req?.body);
-  return res.status(200).json(data);
+  try {
+    console.log("Request Body for Init Endpoint :: ", JSON.stringify(req?.body));
+    const { data } = await initService(req?.body);
+    return res.status(200).json(data);
+  } catch(error: any) {
+    console.log("Error for Init Endpoint :: ", error);
+    return res.status(error?.response?.status || 500).json({ 
+      error: error?.response?.statusText || "An exception has occurred."
+    });
+  }
 };
 
 export const confirm = async (
@@ -47,8 +64,16 @@ export const confirm = async (
   res: Response,
   next: NextFunction
 ) => {
-  const { data } = await confirmService(req?.body);
-  return res.status(200).json(data);
+  try {
+    console.log("Request Body for Confirm Endpoint :: ", JSON.stringify(req?.body));
+    const { data } = await confirmService(req?.body);
+    return res.status(200).json(data);
+  } catch(error: any) {
+    console.log("Error for Confirm Endpoint :: ", error);
+    return res.status(error?.response?.status || 500).json({ 
+      error: error?.response?.statusText || "An exception has occurred."
+    });
+  }
 };
 
 export const status = async (
@@ -56,8 +81,16 @@ export const status = async (
   res: Response,
   next: NextFunction
 ) => {
-  const { data } = await statusService(req?.body);
-  return res.status(200).json(data);
+  try {
+    console.log("Request Body for Status Endpoint :: ", JSON.stringify(req?.body));
+    const { data } = await statusService(req?.body);
+    return res.status(200).json(data);
+  } catch(error: any) {
+    console.log("Error for Status Endpoint :: ", error);
+    return res.status(error?.response?.status || 500).json({ 
+      error: error?.response?.statusText || "An exception has occurred."
+    });
+  }
 };
 
 export const cancel = async (
@@ -65,8 +98,16 @@ export const cancel = async (
   res: Response,
   next: NextFunction
 ) => {
-  const { data } = await cancelService(req?.body);
-  return res.status(200).json(data);
+  try {
+    console.log("Request Body for Cancel Endpoint :: ", JSON.stringify(req?.body));
+    const { data } = await cancelService(req?.body);
+    return res.status(200).json(data);
+  } catch(error: any) {
+    console.log("Error for Cancel Endpoint :: ", error);
+    return res.status(error?.response?.status || 500).json({ 
+      error: error?.response?.statusText || "An exception has occurred."
+    });
+  }
 };
 
 export const track = async (
@@ -74,8 +115,16 @@ export const track = async (
   res: Response,
   next: NextFunction
 ) => {
-  const { data } = await trackService(req?.body);
-  return res.status(200).json(data);
+  try {
+    console.log("Request Body for Track Endpoint :: ", JSON.stringify(req?.body));
+    const { data } = await trackService(req?.body);
+    return res.status(200).json(data);
+  } catch(error: any) {
+    console.log("Error for Track Endpoint :: ", error);
+    return res.status(error?.response?.status || 500).json({ 
+      error: error?.response?.statusText || "An exception has occurred."
+    });
+  }
 };
 
 export const support = async (
@@ -83,8 +132,16 @@ export const support = async (
   res: Response,
   next: NextFunction
 ) => {
-  const { data } = await supportService(req?.body);
-  return res.status(200).json(data);
+  try {
+    console.log("Request Body for Support Endpoint :: ", JSON.stringify(req?.body));
+    const { data } = await supportService(req?.body);
+    return res.status(200).json(data);
+  } catch(error: any) {
+    console.log("Error for Support Endpoint :: ", error);
+    return res.status(error?.response?.status || 500).json({ 
+      error: error?.response?.statusText || "An exception has occurred."
+    });
+  }
 };
 
 export const rating = async (
@@ -92,8 +149,16 @@ export const rating = async (
   res: Response,
   next: NextFunction
 ) => {
-  const { data } = await ratingService(req?.body);
-  return res.status(200).json(data);
+  try {
+    console.log("Request Body for Rating Endpoint :: ", JSON.stringify(req?.body));
+    const { data } = await ratingService(req?.body);
+    return res.status(200).json(data);
+  } catch(error: any) {
+    console.log("Error for Rating Endpoint :: ", error);
+    return res.status(error?.response?.status || 500).json({ 
+      error: error?.response?.statusText || "An exception has occurred."
+    });
+  }
 };
 
 export const update = async (
@@ -102,10 +167,11 @@ export const update = async (
   next: NextFunction
 ) => {
   try {
+    console.log("Request Body for Update Endpoint :: ", JSON.stringify(req?.body));
     const { data } = await updateService(req?.body);
     return res.status(200).json(data);
   } catch(error: any) {
-    console.log("Controler exception => ", error);
+    console.log("Error for Update Endpoint :: ", error);
     return res.status(error?.response?.status || 500).json({ 
       error: error?.response?.statusText || "An exception has occurred."
     });
--- src/DHP/services.ts
+++ src/DHP/services.ts
@@ -26,135 +26,132 @@ const gatewayUrl = process.env.GATEWAY_URL;
 export const searchService = async (body: any) => {
   try {
     const { payload } = buildSearchRequest(body);
-    console.log("Payload for BAP Connection:==>", JSON.stringify(payload));
+    console.log("Payload for Search :: ", JSON.stringify(payload));
     const headers = { "Content-Type": "application/JSON" };
 
     const searchRes = await axios.post(`${gatewayUrl}/search`, payload, {
       headers
     });
+    console.log("Response for Search :: ", JSON.stringify(searchRes?.data));
     const { data } = buildSearchResponse(searchRes.data);
     return { payload, data };
   } catch (error: any) {
-    console.log(error.message);
-    return { errorOccured: true, error: error.message };
+    throw error
   }
 };
 
 export const selectService = async (body: any) => {
   try {
     const { payload } = buildSelectRequest(body);
-    console.log("Payload for Select :==>", JSON.stringify(payload));
+    console.log("Payload for Select :: ", JSON.stringify(payload));
     const headers = { "Content-Type": "application/JSON" };
 
     const selectRes = await axios.post(`${gatewayUrl}/select`, payload, {
       headers
     });
-    // console.log("selectRes => ", selectRes.data);
+    console.log("Response for Select :: ", JSON.stringify(selectRes?.data));
     const { context, orderDetails } = buildSelectResponse(selectRes.data);
-    return { data: { context, orderDetails }, errorOccured: false };
+    return { data: { context, orderDetails } };
   } catch (error: any) {
-    console.log(error);
-    return {
-      status: error?.response?.status || 500,
-      message: error?.response?.statusText || "An exception has occurred."
-    }
+    throw error
   }
 };
 
 export const initService = async (body: any) => {
   try {
     const { payload } = buildInitRequest(body);
-    console.log("Payload for BAP Connection:==>", JSON.stringify(payload));
+    console.log("Payload for Init :: ", JSON.stringify(payload));
     const headers = { "Content-Type": "application/JSON" };
 
     const initRes = await axios.post(`${gatewayUrl}/init`, payload, {
       headers
     });
+    console.log("Response for Init :: ", JSON.stringify(initRes?.data));
     const { context, orderDetails } = buildInitResponse(initRes.data);
     return { data: { context, orderDetails }, errorOccured: false };
   } catch (error: any) {
-    console.log(error.response.data.error.data.errors);
-    return { errorOccured: true, message: error.message };
+    throw error
   }
 };
 
 export const confirmService = async (body: any) => {
   try {
     const { payload } = buildConfirmRequest(body);
-    console.log("Payload for BAP Connection:==>", JSON.stringify(payload));
+    console.log("Payload for Confirm :: ", JSON.stringify(payload));
     const headers = { "Content-Type": "application/JSON" };
 
-    const initRes = await axios.post(`${gatewayUrl}/confirm`, payload, {
+    const confirmRes = await axios.post(`${gatewayUrl}/confirm`, payload, {
       headers
     });
-    const { context, orderDetails } = buildConfirmResponse(initRes.data);
+    console.log("Response for Confirm :: ", JSON.stringify(confirmRes?.data));
+    const { context, orderDetails } = buildConfirmResponse(confirmRes.data);
     return { data: { context, orderDetails }, errorOccured: false };
   } catch (error: any) {
-    console.log(error);
-    return { errorOccured: true, message: error.message };
+    throw error
   }
 };
 
 export const statusService = async (body: any) => {
   try {
     const { payload } = buildStatusRequest(body);
-    console.log("Payload for BAP Connection:==>", JSON.stringify(payload));
+    console.log("Payload for Status :: ", JSON.stringify(payload));
     const headers = { "Content-Type": "application/JSON" };
 
     const statusRes = await axios.post(`${gatewayUrl}/status`, payload, {
       headers
     });
+    console.log("Response for Status :: ", JSON.stringify(statusRes?.data));
     const { context, order } = buildStatusResponse(statusRes.data);
     return { data: { context, order }, errorOccured: false };
   } catch (error: any) {
-    console.log(error);
-    return { errorOccured: true, message: error.message };
+    throw error
   }
 };
 
 export const cancelService = async (body: any) => {
   try {
     const { payload } = buildCancelRequest(body);
-    console.log("Payload for BAP Connection:==>", JSON.stringify(payload));
+    console.log("Payload for Cancel :: ", JSON.stringify(payload));
     const headers = { "Content-Type": "application/JSON" };
 
-    const statusRes = await axios.post(`${gatewayUrl}/cancel`, payload, {
+    const cancelRes = await axios.post(`${gatewayUrl}/cancel`, payload, {
       headers
     });
-    const { context, orderDetails } = buildCancelResponse(statusRes.data);
+    console.log("Response for Cancel :: ", JSON.stringify(cancelRes?.data));
+    const { context, orderDetails } = buildCancelResponse(cancelRes.data);
     return { data: { context, orderDetails }, errorOccured: false };
   } catch (error: any) {
-    console.log(error.response.data.error.data.errors);
-    return { errorOccured: true, message: error.message };
+    throw error
   }
 };
 
 export const updateService = async (body: any) => {
   try {
     const { payload } = buildUpdateRequest(body);
-    console.log("Payload for BAP Connection:==>", JSON.stringify(payload));
+    console.log("Payload for Update :: ", JSON.stringify(payload));
     const headers = { "Content-Type": "application/JSON" };
 
     const updateRes = await axios.post(`${gatewayUrl}/update`, payload, {
       headers
     });
+    console.log("Response for Update :: ", JSON.stringify(updateRes?.data));
     const { context, orderDetails } = buildUpdateResponse(updateRes.data);
     return { data: { context, orderDetails }, errorOccured: false };
   } catch (error: any) {
-    console.log(error);
-    return { errorOccured: true, message: error.message };
+    throw error
   }
 };
 
 export const supportService = async (body: any) => {
   try {
     const { payload } = buildSupportRequest(body);
-    console.log("Payload for BAP Connection:==>", JSON.stringify(payload));
+    console.log("Payload for Support :: ", JSON.stringify(payload));
     const headers = { "Content-Type": "application/JSON" };
 
     const supportRes = await axios.post(`${gatewayUrl}/support`, payload, {
       headers
     });
+    console.log("Response for Support :: ", JSON.stringify(supportRes?.data));
     const { context, orderDetails, supportDetails } = buildSupportResponse(
       supportRes.data
     );
@@ -163,41 +160,40 @@ export const supportService = async (body: any) => {
       errorOccured: false
     };
   } catch (error: any) {
-    console.log(error.response.data.error.data.errors);
-    return { errorOccured: true, message: error.message };
+    throw error
   }
 };
 
 export const ratingService = async (body: any) => {
   try {
     const { payload } = buildRatingRequest(body);
-    console.log("Payload for BAP Connection:==>", JSON.stringify(payload));
+    console.log("Payload for Rating :: ", JSON.stringify(payload));
     const headers = { "Content-Type": "application/JSON" };
 
     const ratingRes = await axios.post(`${gatewayUrl}/rating`, payload, {
       headers
     });
+    console.log("Response for Rating :: ", JSON.stringify(ratingRes?.data));
     const { context, message } = buildRatingResponse(ratingRes.data);
     return { data: { context, message }, errorOccured: false };
   } catch (error: any) {
-    console.log(error.response.data.error.data.errors);
-    return { errorOccured: true, message: error.message };
+    throw error
   }
 };
 
 export const trackService = async (body: any) => {
   try {
     const { payload } = buildTrackRequest(body);
-    console.log("Payload for BAP Connection:==>", JSON.stringify(payload));
+    console.log("Payload for Track :: ", JSON.stringify(payload));
     const headers = { "Content-Type": "application/JSON" };
 
-    const ratingRes = await axios.post(`${gatewayUrl}/track`, payload, {
+    const trackRes = await axios.post(`${gatewayUrl}/track`, payload, {
       headers
     });
-    const { context, trackingDetails } = buildTrackResponse(ratingRes.data);
+    console.log("Response for Track :: ", JSON.stringify(trackRes?.data));
+    const { context, trackingDetails } = buildTrackResponse(trackRes.data);
     return { data: { context, trackingDetails }, errorOccured: false };
   } catch (error: any) {
-    console.log(error.response.data.error.data.errors);
-    return { errorOccured: true, message: error.message };
+    throw error
   }
 };
